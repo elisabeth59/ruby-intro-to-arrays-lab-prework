142b2aef9fb0e821cc6728495b1e6cc13998cdb2
Elisabeth Narine <elisabethnarine@gmail.com>
2018-05-06 19:00:45 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -35,5 +35,5 @@ end
 
 def length_of_array(length)
 @programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]+
-  length = length_of_array(8)
+  length = length_of_array(@programming_languages).8
 end
